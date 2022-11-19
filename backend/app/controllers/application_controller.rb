5563edb02e3548eea26f71d1387c4ce64b4e4992
Nick <nfears5798@gmail.com>
2022-11-19 15:16:58 -0500

    updates

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,9 +1,83 @@
+require 'rest-client'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/' do
+    "hello world"
   end
 
+  # Add your routes here
+  get "/companies" do
+    companies = Company.all
+    companies.to_json
+  end
+
+  get '/companies/:id' do
+    comp = Company.find(params[:id])
+    comp.to_json
+  end
+
+  get "/investors" do
+    investors = Investor.all
+    investors.to_json
+  end
+
+  get "/shares" do
+    shares = Share.all
+    shares.to_json
+  end
+
+  get '/shares/:id' do
+    share = Share.id
+    share.to_json
+  end
+
+  post '/companies' do
+    new_company = Company.create(
+      name: params[:name],
+      price: params[:price],
+      ticker: params[:stock_ticker]
+    )
+    new_company.to_json
+  end
+
+  get "/companies/by_price" do
+    company_price = Company.by_price
+    company_price.to_json
+  end
+
+  get "/companies/most_expensive" do
+    company_max= Company.most_expensive
+    company_max.to_json
+  end
+
+  get "/companies/least_expensive" do
+    company_min= Company.most_expensive
+    company_min.to_json
+  end
+
+  post '/investors/:id' do
+  investor_add = Investor.find(params[:id])
+  investor_add.create(
+     name: params[:name]
+    )
+  investor_add.to_json
+  end
+
+
+  patch '/investors/:id' do
+    investor_edit = Investor.find(params[:id])
+    investor_edit.update(
+       name: params[:name],
+      )
+    investor_edit.to_json
+    end
+
+
+    delete '/investors/:id' do
+      d_inv = Investor.find(params[:id])
+      d_inv.destroy
+      d_inv.to_json
+    end
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,28 +1,25 @@
+require 'rest-client'
 puts "🌱 Seeding spices..."
 
 # Seed your database here
 def get_companies 
-    companies = RestClient.get "https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&sector=Technology&exchange=NASDAQ&dividendMoreThan=0&limit=100&apikey=345d03e54402c34a9096c537f3b36196"
+    companies = RestClient.get("https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&sector=Technology&exchange=NASDAQ&dividendMoreThan=0&limit=100&apikey=345d03e54402c34a9096c537f3b36196")
     companies_hash = JSON.parse(companies)
-    companies_hash["symbol"].each do |company|
+    companies_hash[0]["symbol"].each do |company|
         Company.find_or_create_by(stock_ticker: company)
     end 
 end
 
-Company.all.each do |poet|
-    response = RestClient.get "https://financialmodelingprep.com/api/v3/profile/#{company.symbol}?apikey=345d03e54402c34a9096c537f3b36196"
+Company.all.each do |company|
+    response = RestClient.get("https://financialmodelingprep.com/api/v3/profile/#{company.stock_ticker}?apikey=345d03e54402c34a9096c537f3b36196")
     companies_hash = JSON.parse(response)
     companies_hash.each do |company|
-        company.companies.create(
-            name: company["companyName"],
-            price: company["price"]
-        )
+        company.create (
+            company["companyName"]
+            # company[price] 
+            )
     end
     print "."
 end 
-end
 
 get_companies
-
-
-puts "✅ Done see
